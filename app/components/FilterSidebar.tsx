8589f4509ac8feefc492b775b69530c84fc3c486
Drikus Roor <drikusroor@gmail.com>
2025-01-28 20:42:28 +0100

    Refactor: Remove unused useCallback import from FilterSidebar component

--- app/components/FilterSidebar.tsx
+++ app/components/FilterSidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useCallback, useRef } from 'react';
+import React, { useRef } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 
 export default function FilterSidebar({ facets }: { facets: any[] }) {
